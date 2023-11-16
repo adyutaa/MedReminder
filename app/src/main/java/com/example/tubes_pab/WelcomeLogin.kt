2118fd33d1d94dd4d40a842d0b35c4c7fa6d5962
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-11-16 11:33:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tubes_pab/WelcomeLogin.kt
@@ -0,0 +1,11 @@
+package com.example.tubes_pab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_login)
+    }
+}
